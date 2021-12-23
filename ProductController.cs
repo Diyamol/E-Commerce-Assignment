380a0810105ccaa0858fbef476a64c6150787044
Diyamol <79752373+Diyamol@users.noreply.github.com>
2021-12-23 13:58:51 -0500

    controller added

---
+++ ProductController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSharpAssignment2.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpAssignment2.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly ILogger<ProductController> _logger;
+        private MyDatabaseContext context;
+        public ProductController(ILogger<ProductController> logger, MyDatabaseContext ctx)
+        {
+            context = ctx;
+            _logger = logger;
+        }
+        public IActionResult Home()
+        {
+            try
+            {
+                var product = (from t in context.Products select t).Take(5);
+                //var product = context.Products.ToList();
+                return View(product);
+            }
+            catch (Exception e)
+            {
+                return View("Oops, Error getting data from Database!!");
+            }
+        }
+        
+        public async Task<IActionResult> Products( int id)
+        {
+            string name = HttpContext.Session.GetString("uname");
+
+            if (name != "admin")
+            {
+                // productid = Int32.Parse(TempData["id"].ToString());
+                var pid = id;
+                HttpContext.Session.SetString("pid", id.ToString());
+                var product = from prod in context.Products where prod.productId== pid select prod;
+                return View(await product.ToListAsync());
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Products( Products product, string buy, string toCart)
+        {
+            string name = HttpContext.Session.GetString("uname");
+            if (name != null && name != "admin")
+            {
+                if (buy != null)
+                {
+                    return RedirectToAction("Checkout", "User");
+                }
+                else if (toCart != null)
+                {
+                    var user = from usr in context.User where usr.username == name select usr;
+                    foreach (var i in user)
+                    {
+                        int userid = Convert.ToInt32(i.userId);
+                        string pid = HttpContext.Session.GetString("pid");
+                        int id = Convert.ToInt32(pid);
+                        var p = from prod in context.Products where prod.productId == id select prod;
+                        foreach (var item in p)
+                        {
+                            Cart cart = new Cart
+                            {
+                                price = item.price,
+                                quantity = 1,
+                                productId = item.productId,
+                                userId = userid,
+                                status = "added to cart"
+                            };
+                            context.Cart.Add(cart);
+                        }
+                    }
+                    context.SaveChanges();
+                    return RedirectToAction("Cart", "User");
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+       
+        }
+        
+    }
+}
---
+++ UserController.cs
@@ -0,0 +1,137 @@
+﻿using CSharpAssignment2.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment2.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private MyDatabaseContext context { get; set; }
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public UserController(MyDatabaseContext ctx, IWebHostEnvironment hostEnv)
+        {
+            context = ctx;
+            webHostEnvironment = hostEnv;
+        }
+        public IActionResult UserHome()
+        {
+            string name = HttpContext.Session.GetString("uname");
+
+            if (name != "admin")
+            {
+                if (name != "" && name != null)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        public async Task<IActionResult> Cart(Cart cart)
+        {
+            string name = HttpContext.Session.GetString("uname");
+
+            if (name != "admin")
+            {
+                if (name != "" && name != null)
+                {
+                    var user = from usr in context.User where usr.username == name select usr;
+                    foreach (var item in user)
+                    {
+                        var id = item.userId;
+                        var product = from c in context.Cart where c.userId == id select c;
+                        return View(await product.ToListAsync());
+                    }
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        public IActionResult Checkout()
+        {
+            string name = HttpContext.Session.GetString("uname");
+
+            if (name != "admin")
+            {
+                if (name != "" && name != null)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        public IActionResult Orders()
+        {
+            string name = HttpContext.Session.GetString("uname");
+
+            if (name != "admin")
+            {
+                if (name != "" && name != null)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        public IActionResult Success()
+        {
+            string name = HttpContext.Session.GetString("uname");
+
+            if (name != "admin")
+            {
+                if (name != "" && name != null)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+    }
+}
